938f345e6fad7058262abc3845328dd7f22f4d61
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-06-27 20:50:30 +0200

    add calculator tests and code

--- calculator.js
+++ calculator.js
@@ -1,5 +1,21 @@
-function calculator(operation, firstNumber, secondNumber){
-    return 
+//A calculator object that contains functions for the basic operations:
+// add, subtract, divide, and multiply. Each of these functions
+//should take two numbers and return the correct calculation.
+
+function calculator(operation, a, b){
+    let result = null
+
+    if (operation === 'addition'){
+        result = Number(a) + Number(b);
+    } else if (operation === 'subtraction'){
+        result = Number(a) - Number(b);
+    } else if (operation === 'division'){
+        result = Number(a) / Number(b);
+    } else if (operation === 'multiplication'){
+        result = Number(a) * Number(b);
+    }
+
+    return Math.round(result*100000000000000)/100000000000000
 }
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -1,1 +1,49 @@
 const calculator = require('./calculator.js')
+
+test('addition one', ()=>{
+    expect(calculator('addition', 1, 2)).toBe(3)
+})
+
+test('addition two', ()=>{
+    expect(calculator('addition', 0.1, 0.2)).toBe(0.3)
+})
+
+test('addition three', ()=>{
+    expect(calculator('addition', -123, 23)).toBe(-100)
+})
+
+test('subtraction one', ()=>{
+    expect(calculator('subtraction', 1, 2)).toBe(-1)
+})
+
+test('subtraction one', ()=>{
+    expect(calculator('subtraction', 13, 2)).toBe(11)
+})
+
+test('subtraction one', ()=>{
+    expect(calculator('subtraction', 0.1, 0.2)).toBe(-0.1)
+})
+
+test('division one', ()=>{
+    expect(calculator('division', 1, 2)).toBe(0.5)
+})
+
+test('division one', ()=>{
+    expect(calculator('division', -1, 2)).toBe(-0.5)
+})
+
+test('division one', ()=>{
+    expect(calculator('division', -100, -2)).toBe(50)
+})
+
+test('multiplication one', ()=>{
+    expect(calculator('multiplication', 4, 2)).toBe(8)
+})
+
+test('multiplication two', ()=>{
+    expect(calculator('multiplication', 12, -4)).toBe(-48)
+})
+
+test('multiplication three', ()=>{
+    expect(calculator('multiplication', 0.2, 3)).toBe(0.6)
+})
